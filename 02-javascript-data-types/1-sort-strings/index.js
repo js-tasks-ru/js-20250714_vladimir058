c8c7487f872d82c7bb13d9da4d91c0dc7a24069d
1rb1s2312 <DanilovVladimir@ya.ru>
2025-08-07 11:34:06 +0300

    решение задачи сортировки 2.1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,46 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+  /*const collator = new Intl.Collator("ru-RU", {
+    sensitivity: "base",
+    usage: "sort",
+  });
 
+  return [...arr].sort((a, b) => {
+    let result = collator.compare(a, b);
+
+    if (param === "desc") {
+      result *= -1;
+    }
+    //Возвращаем результат
+    return result;
+  });
+  */
+  const arrSortLocate = arr.slice().sort((a, b) => {
+    const comparisonResult = a.localeCompare(b, undefined, {
+      sensitivity: "base",
+      usage: "sort",
+    });
+
+    if (param === "asc") {
+      return comparisonResult;
+    } else {
+      return -comparisonResult;
+    }
+  });
+
+  return arrSortLocate.sort((a, b) => {
+    const isUpperA = a !== a.toLowerCase();
+    const isUpperB = b !== b.toLowerCase();
+    if (a.toLowerCase() !== b.toLowerCase()) {
+      return 0;
+    }
+    if (isUpperA && !isUpperB) {
+      return -1;
+    }
+    if (!isUpperA && isUpperB) {
+      return +1;
+    }
+  });
 }
