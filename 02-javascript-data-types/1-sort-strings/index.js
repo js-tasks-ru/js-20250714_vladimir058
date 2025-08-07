e0bba9c1360f2d321d7039ef73d087576d5e192c
1rb1s2312 <DanilovVladimir@ya.ru>
2025-08-07 13:17:33 +0300

    указывание явно локаля и рефакторинг

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,24 +5,10 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = "asc") {
-  /*const collator = new Intl.Collator("ru-RU", {
-    sensitivity: "base",
-    usage: "sort",
-  });
-
-  return [...arr].sort((a, b) => {
-    let result = collator.compare(a, b);
-
-    if (param === "desc") {
-      result *= -1;
-    }
-    //Возвращаем результат
-    return result;
-  });
-  */
-  const arrSortLocate = arr.slice().sort((a, b) => {
-    const comparisonResult = a.localeCompare(b, undefined, {
-      sensitivity: "base",
+  return arr.slice().sort((a, b) => {
+    const comparisonResult = a.localeCompare(b, ["ru", "en"], {
+      caseFirst: "upper",
+      //sensitivity: "base",
       usage: "sort",
     });
 
@@ -30,20 +16,6 @@ export function sortStrings(arr, param = "asc") {
       return comparisonResult;
     } else {
       return -comparisonResult;
-    }
-  });
-
-  return arrSortLocate.sort((a, b) => {
-    const isUpperA = a !== a.toLowerCase();
-    const isUpperB = b !== b.toLowerCase();
-    if (a.toLowerCase() !== b.toLowerCase()) {
-      return 0;
-    }
-    if (isUpperA && !isUpperB) {
-      return -1;
-    }
-    if (!isUpperA && isUpperB) {
-      return +1;
     }
   });
 }
