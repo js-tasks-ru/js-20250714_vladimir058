a036f46df792a76215821f24503a0c00f606f148
1rb1s2312 <DanilovVladimir@ya.ru>
2025-08-18 02:10:50 +0300

    Задание 3.2 и 3.3 - создание функций invertObject и trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,31 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (typeof string !== "string") {
+    return undefined;
+  }
+  if (typeof size == "undefined") {
+    return string;
+  }
+  const startCount = 0;
+  let resultString = "";
+  let symbolCurrent = "";
+  let symbolCount = startCount;
+  if (size <= 0) {
+    return resultString;
+  }
+  for (let i = 0; i < string.length; i++) {
+    if (symbolCurrent == string[i]) {
+      symbolCount++;
+    } else {
+      symbolCurrent = string[i];
+      symbolCount = 1;
+    }
 
+    if (symbolCount <= size) {
+      resultString += string[i];
+    }
+  }
+
+  return resultString;
 }
