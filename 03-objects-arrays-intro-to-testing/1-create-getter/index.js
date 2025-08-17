616b84eee81bb4d6000f0f77c61ad5bf49de1e6e
1rb1s2312 <DanilovVladimir@ya.ru>
2025-08-18 00:39:52 +0300

    Задание 3.1 - создание функции createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const keys = path.split(".");
 
+  return (object) => {
+    let result = object;
+    for (let key of keys) {
+      if (
+        !result ||
+        typeof result !== "object" ||
+        !Object.getOwnPropertyNames(result).includes(key)
+        //!(key in result)
+      ) {
+        return undefined;
+      }
+      result = result[key];
+    }
+    return result;
+  };
 }
