fe0fc3ac4e5ba6d304ea9cf25d848d352caa9570
1rb1s2312 <DanilovVladimir@ya.ru>
2025-11-21 03:24:52 +0300

    Задание 5.1 - Создание компонента для отображения сообщений

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
+  constructor(message, options = {}) {
+    this.message = message;
+    this.duration = options.duration || 2000;
+    this.type = options.type || "success";
+    this.timer = null;
+    this.element = this.createElement();
+  }
 
+  createElement() {
+    const container = document.createElement("div");
+    container.classList.add("notification", this.type);
+    container.setAttribute("style", "--value:" + this.duration / 1000 + "s");
+
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("inner-wrapper");
+
+    const header = document.createElement("div");
+    header.classList.add("notification-header");
+    header.textContent = this.type;
+
+    const body = document.createElement("div");
+    body.classList.add("notification-body");
+    body.textContent = this.message;
+
+    wrapper.append(header, body);
+    container.append(wrapper);
+
+    return container;
+  }
+
+  show(targetElement = document.body) {
+    const existingNotifications = document.querySelectorAll(".notification");
+    for (let i = 0; i < existingNotifications.length; i++) {
+      existingNotifications[i].parentNode.removeChild(existingNotifications[i]);
+    }
+
+    targetElement.appendChild(this.element);
+
+    this.timer = setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+  }
+
+  remove() {
+    if (this.element && this.element.parentNode) {
+      this.element.parentNode.removeChild(this.element);
+      this.element = null;
+    }
+  }
+
+  destroy() {
+    this.remove();
+
+    if (this.timer) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
+  }
 }
